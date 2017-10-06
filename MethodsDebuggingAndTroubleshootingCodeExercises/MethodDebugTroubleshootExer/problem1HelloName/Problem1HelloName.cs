dcf1696bccb1e46541f6540bbf742f42205d563c
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-06 21:39:56 +0300

    newprogekt

---
+++ MethodsDebuggingAndTroubleshootingCodeExercises/MethodDebugTroubleshootExer/problem1HelloName/Problem1HelloName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace problem1HelloName
+{
+    class Problem1HelloName
+    {
+        static void printName(string b , string a)
+        {
+            Console.WriteLine($"{b} {a}!");
+        }
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            string pozdrav = "Hello,";
+            printName(pozdrav,name);
+        }
+    }
+}
---
+++ MethodsDebuggingAndTroubleshootingCodeExercises/MethodDebugTroubleshootExer/problem2MaxMethod/Problem2MaxMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace problem2MaxMethod
+{
+    class Problem2MaxMethod
+    {
+        static void GetMax(int a, int b, int c)
+            {
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(c);
+            }
+            }
+
+        static void Main()
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secNum = int.Parse(Console.ReadLine());
+            int thrtNum = int.Parse(Console.ReadLine());
+            GetMax(firstNum, secNum, thrtNum);
+        }
+    }
+}
